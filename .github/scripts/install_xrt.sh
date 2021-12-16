379d0eece26310a8ce39130cecf82b38c2e47482
P Eshwar Prasad <84436961+eshwar-xilinx@users.noreply.github.com>
2021-12-16 15:53:37 +0530

    Update install_xrt.sh

--- .github/scripts/install_xrt.sh
+++ .github/scripts/install_xrt.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
 CWD=$(readlink -f .)
-#####################################################################################################
-BDF1=$(lspci -d 10ee: | awk '{print $1}' | awk ' FNR == 1')
-DEVID1=$(lspci -d 10ee: | awk '{print $7}' | awk ' FNR == 1')
-
-BDF2=$(lspci -d 10ee: | awk '{print $1}' | awk ' FNR == 2')
-DEVID2=$(lspci -d 10ee: | awk '{print $7}' | awk ' FNR == 2')
-
-DISTRO=$(grep '^ID=' /etc/os-release | awk -F= '{print $2}' | tr -d '"')
-
-FLAG=0
-
-if [[ $DEVID1 == "5001" || $DEVID1 == "5005" || $DEVID1 == "5021" ]]; then
-        USERPF=$BDF1
-        MGMTPF=$BDF2
-else
-        USERPF=$BDF2
-        MGMTPF=$BDF1
-fi
-
-###############################################################################
-##                       checking distro 				     ##
-###############################################################################
+RETURN_CODE=0
+DISTRO=`grep '^ID=' /etc/os-release | awk -F= '{print $2}' | tr -d '"'`
 
 cd $CWD/XRT/build/Release
 if [[ $DISTRO == "centos" ]]; then
@@ -38,13 +18,13 @@ if [[ $DISTRO == "centos" ]]; then
                 DRIVER=`lsmod | grep -i xocl`
                 if [ -z "$DRIVER" ]; then
                         echo "Drivers are NOT loaded"
-                        FLAG=1
+                        RETURN_CODE=1
                 else
                         echo "Drivers are loaded"
                 fi
         else
                 echo "Package XRT is NOT installed!"
-		FLAG=1
+		RETURN_CODE=1
         fi
 else
 	dpkg -s xrt  &> /dev/null
@@ -53,45 +33,16 @@ else
 		DRIVER=`lsmod | grep -i xocl`
 		if [ -z "$DRIVER" ]; then
 			echo "Drivers are NOT loaded"
-		 	FLAG=1
+		 	RETURN_CODE=1
 		else
       echo "Drivers are loaded"
     		fi
 	else
 		echo "Package XRT is NOT installed!"
-		FLAG=1
+		RETURN_CODE=1
 	fi
 fi
 
-###############################################################################
-#  installing platform packages
-###############################################################################
-
-
-if [[ $DEVID1 != "5005" ]]; then	#for u250 card, shell needs to be updated
-        if [[ $DISTRO == "centos" ]]; then
-		cd $CWD/u*
-                sudo yum install -y ./xilinx*.rpm
-        else
-		sudo rm -rf $CWD/ubuntu* > /dev/null 2>&1
-		cd $CWD/u*
-                sudo apt install ./xilinx*.deb
-        fi
-else
-	sudo rm -rf $CWD/ubuntu* > /dev/null 2>&1
-        cd $CWD/u*
-        sudo apt install ./xilinx-cmc*.deb
-        sudo apt install ./xilinx-sc-fw*.deb
-        sudo apt install ./xilinx*base*.deb
-        sudo /opt/xilinx/xrt/bin/xbmgmt --base --device $MGMT_PF
-        sudo apt install ./xilinx*validate*.deb
-        sudo apt install ./xilinx*shell*.deb
-        sudo /opt/xilinx/xrt/bin/xbmgmt program --shell /opt/xilinx/firmware/u250/gen3x16/xdma-shell/partition.xsabin --device $MGMT_PF
-
-fi
-
 cat `find /var/lib/dkms/xrt/ -iname "make.log"`
-if [ $FLAG == 1 ]; then
-	exit 1
-fi
+exit $RETURN_CODE
 
