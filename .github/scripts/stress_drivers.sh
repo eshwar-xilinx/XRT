b8a6f474034b0a6be3b75236628a385dcaafee92
P Eshwar Prasad <84436961+eshwar-xilinx@users.noreply.github.com>
2021-12-17 17:06:35 +0530

    Update stress_drivers.sh

--- .github/scripts/stress_drivers.sh
+++ .github/scripts/stress_drivers.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-x=1
+sudo rmmod xocl && sudo rmmod xclmgmt
+sudo modprobe xocl && sudo modprobe xclmgmt
 
-while [ $x -le 50 ]
-do
-	sudo rmmod xocl && sudo rmmod xclmgmt
-	sudo modprobe xocl && sudo modprobe xclmgmt
-	x=$(( $x + 1 ))
-done
+sudo rmmod xocl && sudo rmmod xclmgmt
+sudo modprobe xocl && sudo modprobe xclmgmt
 
 dmesg
 
