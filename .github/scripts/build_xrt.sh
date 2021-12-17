8c5ab1a0ccb34d157f346228e25a9b75756de38a
P Eshwar Prasad <84436961+eshwar-xilinx@users.noreply.github.com>
2021-12-17 17:09:51 +0530

    Update build_xrt.sh

--- .github/scripts/build_xrt.sh
+++ .github/scripts/build_xrt.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-PR_ID=$1
-CWD=$(readlink -f .)
 DISTRO=$(grep '^ID=' /etc/os-release | awk -F= '{print $2}' | tr -d '"')
 
-sudo dmesg -C
-
-sudo $CWD/XRT/src/runtime_src/tools/scripts/xrtdeps.sh 
+sudo ./src/runtime_src/tools/scripts/xrtdeps.sh 
 
 if [[ $DISTRO == "centos" ]]; then
         source /opt/rh/devtoolset-*/enable
@@ -15,4 +11,4 @@ fi
 export XILINX_VITIS=/mnt/packages/vitis
 export PATH=/mnt/packages/vitis/gnu/microblaze/lin/bin:/mnt/packages/vitis/gnu/microblaze/linux_toolchain/lin64_le/bin:$PATH
 
-$CWD/XRT/build/build.sh -opt
+./build/build.sh -opt
